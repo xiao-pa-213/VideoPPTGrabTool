727d49fb1976d1612ea8d54cd8a4b68b3ae04a56
xiao-pa-213 <1328221299@qq.com>
2024-05-01 15:48:29 +0800

    add check_path(ts_file_path)

--- src/check_paths.py
+++ src/check_paths.py
@@ -11,6 +11,7 @@ def check_all_paths():
     check_path(output_ppt_path)
     check_path(template_path)
     check_path(pictemp_path)
+    check_path(ts_file_path)
     print("Check finished")
 
 def main():
